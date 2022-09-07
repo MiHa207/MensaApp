be6deb0cdef2b5189ada6faf558f2c57ac2474e1
Ploxbot <33115746+MiHa207@users.noreply.github.com>
2022-09-07 17:11:57 +0200

    removed tailwind

--- App.js
+++ App.js
@@ -1,17 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
-import { TailwindProvider } from 'tailwindcss-react-native';
-
+import { TailwindProvider } from "tailwindcss-react-native";
 
 export default function App() {
   return (
-    <TailwindProvider>
     <View className="flex-1 items-center justify-center bg-white">
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-    </TailwindProvider>
   );
 }
-
-
