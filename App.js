02f12a2697037a856bdd881ff82b7565240b075e
Ploxbot <33115746+MiHa207@users.noreply.github.com>
2022-09-11 18:54:45 +0200

    Added Navigation + Screens + Buttons

--- App.js
+++ App.js
@@ -1,19 +1,86 @@
 import { StyleSheet, Text, View, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+// NAVIGATION
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headTxt}>Mensa App</Text>
-      </View>
-      <View>
-        <Button style={styles.buttontxt} title="Kalender" />
-        <Button title="Essensplan" />
-        <Button title="Gerichte" />
-      </View>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "MensaApp" }}
+        />
+        <Stack.Screen
+          name="Kalender"
+          component={CalenderScreen}
+          options={{ title: "Kalenderwoche" }}
+        />
+        <Stack.Screen
+          name="Gerichte"
+          component={DishesScreen}
+          options={{ title: "Gerichte" }}
+        />
+        <Stack.Screen
+          name="Essensplan"
+          component={PlanScreen}
+          options={{ title: "Essenplan" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+// SCREENS
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Button
+        title="Kalender"
+        onPress={() => navigation.navigate("Kalender")}
+      />
+      <Button
+        title="Gerichte"
+        onPress={() => navigation.navigate("Gerichte")}
+      />
+      <Button
+        title="Essensplan"
+        onPress={() => navigation.navigate("Essensplan")}
+      />
+    </View>
+  );
+};
+
+const CalenderScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Kalender</Text>
+    </View>
+  );
+};
+
+const DishesScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Gerichte</Text>
+    </View>
+  );
+};
+
+const PlanScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Essensplan</Text>
+    </View>
+  );
+};
+
+// STYLES
 
 const styles = StyleSheet.create({
   container: {
